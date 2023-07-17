b7e6974f652c6e58d7bde2fa3020661b7c28eb1d
Kiran Bansal <136809550+Kiran-bansal@users.noreply.github.com>
2023-07-17 13:33:06 +0530

    Create sametree.py

---
+++ sametree.py
@@ -0,0 +1,5 @@
+class Solution(object):
+    def isSameTree(self, p, q):
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p is q

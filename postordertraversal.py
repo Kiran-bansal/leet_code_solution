e434c3c81aa2f7f60f48891ad6f465628c5df7ac
Kiran Bansal <136809550+Kiran-bansal@users.noreply.github.com>
2023-07-21 14:50:07 +0530

    Create postordertraversal.py

---
+++ postordertraversal.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def postorderTraversal(self, root):
+        traversal, stack = [], [(root, False)]
+        while stack:
+            node, visited = stack.pop()
+            if node:
+                if visited:
+                    traversal.append(node.val)
+                else:
+                    stack.append((node, True))
+                    stack.append((node.right, False))
+                    stack.append((node.left, False))
+
+        return traversal
